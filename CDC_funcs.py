25125d457bdb417f44367ed9650cbd2e330f898f
KALT <75012967+kajalthuis@users.noreply.github.com>
2021-08-10 18:30:20 +0200

    Add files via upload

---
+++ CDC_funcs.py
@@ -0,0 +1,214 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 17:52:23 2021
+
+@author: KALT
+"""
+
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import io
+import base64
+
+
+def convert(list_, radio1, radio2, new_name):
+    
+    frame = pd.concat(list_, axis=0, ignore_index=True)
+    
+        ## Rename columns ##
+    frame = frame.rename(columns={0: 'Date',
+                                  1: 'Time',
+                                  2: 'Vessel_name',
+                                  3: 'Level/Pattern',
+                                  4: 'Location',
+                                  5: 'Easting',
+                                  6: 'Northing',
+                                  7: 'Total_settlement',
+                                  8: 'Total_time',
+                                  9: 'Total_energy',
+                                  10: 'Average_velocity',
+                                  11: 'Manual_velocity',
+                                  12: 'Foot_diameter',
+                                  13: 'Stop_criterion',
+                                  14: 'Num_blows',
+                                  15: 'Set_1'})
+    for i in range(16, 76):
+        frame = frame.rename(columns={i:f'Set_{i-14}'})    
+    #####
+    
+    frame = frame.sort_values(['Date', 'Time'])
+    frame = frame.drop(frame.columns[[-1,]], axis=1)
+    del frame['Average_velocity']
+    del frame['Manual_velocity']
+    frame = frame[frame.Date != 'yyyymmdd']
+    
+        ## Add date and time columns ##
+    frame['Date'] = pd.to_datetime(frame['Date'], format='%Y%m%d').dt.date
+    frame['Time'] = pd.to_datetime(frame['Time'], format='%H%M%S').dt.time
+    
+        ## Split Level/Pattern column ##
+    frame.insert(3, 'Level', 0)
+    frame.insert(4, 'Pattern', 0)
+    frame.Level, frame.Pattern = frame['Level/Pattern'].str.split('/').str
+    
+    try:
+        frame.Pattern = frame.Pattern.astype(float)
+    except:
+        pass
+    del frame['Level/Pattern']
+    
+
+    frame_drop = frame.dropna()
+    Nan_rows=frame[~frame.index.isin(frame_drop.index)]
+    frame = frame.dropna()
+        
+        ## Convert columns to float ##
+    frame.Num_blows = frame.Num_blows.astype(int)
+    frame.Location = frame.Location.astype(int)
+    frame.Easting = frame.Easting.astype(float)
+    frame.Northing = frame.Northing.astype(float)
+    frame.Total_settlement = frame.Total_settlement.astype(float)
+    frame.Total_time = frame.Total_time.astype(float)
+    frame.Total_energy = frame.Total_energy.astype(float)
+    frame.Foot_diameter = frame.Foot_diameter.astype(float)
+    frame.Stop_criterion = frame.Stop_criterion.astype(float)
+    frame.Set_1 = frame.Set_1.astype(float)
+    
+        ## Print info and warnings ##
+    
+    blows60 = frame.Num_blows[frame.Num_blows > 60].count()
+    if blows60 > 1:
+        st.write(f'WARNING: {blows60} points with more than 60 blows. This will result in a wrong induced settlement for those points')
+    elif blows60 == 1:
+        st.write(f'WARNING: {blows60} point with more than 60 blows. This will result in a wrong induced settlement for that point')
+    
+    # if empty > 1:
+    #     st.write(empty, 'files are empty')
+    # elif empty == 1:
+    #     st.write(empty, 'file is empty')
+    
+    if len(Nan_rows) > 1:
+        st.write('Removed', len(Nan_rows), 'lines with Nan values')
+    elif len(Nan_rows) == 1:
+        st.write('Removed', len(Nan_rows), 'line with Nan values')
+    
+        ## Remove line with less than 3 blows ## (can be adjusted)
+    if radio1 == 'yes':
+        noblows = frame.Num_blows[frame.Num_blows <= 2].count()
+        if noblows > 1:
+            st.write(f'Removed **{noblows}** points with less than 3 blows')
+        elif noblows ==1:
+            st.write(f'Removed **{noblows}** point with less than 3 blows')
+        
+        frame = frame[frame.Num_blows > 2]
+        
+        if frame.Total_settlement.mean() < 0:
+            frame.Total_settlement = frame.Total_settlement * -1
+            for i in range(1, 61):
+                frame[f'Set_{i}'] = frame[f'Set_{i}'] * -1
+    
+        ## Calculate induced settlement per blow and put it in a new column ##
+    for i in range(4,61):
+        frame[f'ind_{i}'] = ((frame[f'Set_{i-2}'] - frame[f'Set_{i-3}']) + (frame[f'Set_{i-1}'] - frame[f'Set_{i-2}']) + (frame[f'Set_{i}'] - frame[f'Set_{i-1}']))/3
+    
+        ## Remove minus values resulting from calculation of induced settlement ##
+    keys = list(frame)
+    start = keys.index('ind_4')
+    end = keys.index('ind_60')+1
+    for i in range(start,end):
+        for j in range(0,len(frame.ind_4)):
+            if frame.iloc[j,i] < 0:
+                frame.iloc[j,i] = 0
+    
+        ## change to 60 blows if more than 60 blows ## (can be adjusted, but not taken into account for induced. Only 60 blows are imported)       
+    Num_blows_index = keys.index('Num_blows')            
+    for i in range(0, len(frame.Num_blows)):
+        if frame.iloc[i, Num_blows_index] > 60:
+            frame.iloc[i, Num_blows_index] = 60             
+           
+        ## Add a column with induced settlement ##
+    frame['Total_induced'] = 0
+    keys = list(frame)
+    Total_induced_index = keys.index('Total_induced')
+    
+    for i in range(len(frame['Total_induced'])):
+        Blows = frame.iloc[i, Num_blows_index]
+        if Blows > 4:        
+            ind_index = keys.index(f'ind_{Blows}')
+            frame.iloc[i, Total_induced_index] = frame.iloc[i, ind_index-1]
+    
+        ## Add column with cumulative count of points ##
+        
+    Cranes = list(set(frame.Vessel_name))
+    for i in  Cranes:
+        crane = list(frame.Vessel_name)
+        points_per_crane = crane.count(i)
+        st.write('  * Points compacted by', i,':', points_per_crane)
+    
+    st.write("  * Total points compacted: ", len(frame))
+    
+    try:
+        st.write('  * Total square meter covered:', sum(frame.Pattern * frame.Pattern))
+    except:
+        st.write('  * Square meter not possible, no grid entered in cms')
+      
+    #Preview    
+    
+    return frame
+
+
+def download_link_csv(object_to_download, download_filename, download_link_text):
+    """
+    Generates a link to download the given object_to_download.
+
+    object_to_download (str, pd.DataFrame):  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv
+    download_link_text (str): Text to display for download link.
+
+    Examples:
+    download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
+    download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
+
+    """
+    if isinstance(object_to_download, pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=True)
+    
+    b64 = base64.b64encode(object_to_download.encode()).decode()
+
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+
+
+def download_link_excel(object_to_download, download_filename, download_link_text):
+    """
+    Generates a link to download the given object_to_download.
+
+    object_to_download (str, pd.DataFrame):  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv
+    download_link_text (str): Text to display for download link.
+
+    Examples:
+    download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
+    download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
+
+    """
+    if isinstance(object_to_download, pd.DataFrame):
+        towrite = io.BytesIO()
+        object_to_download = object_to_download.to_excel(towrite, index=True)
+        towrite.seek(0)
+    
+    b64 = base64.b64encode(towrite.read()).decode()
+
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+
+
+def show_preview(frame):
+    st.write('**Preview:**')
+    fig = px.scatter(data_frame = frame,
+                     x=frame['Easting'],
+                     y=frame['Northing'],
+                     color='Num_blows', 
+                     color_continuous_scale='turbo')
+                     
+    fig.update_yaxes(scaleanchor='x', scaleratio=1)
+    st.write(fig)
---
+++ CRC_funcs.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  3 14:41:59 2020
+
+@author: KALT
+"""
+
+import pandas as pd
+import math
+import plotly.express as px
+import streamlit as st
+
+    
+def convert(pos_, acc_, log_, log_present, acc_present):
+    ## log ## 
+    
+    if log_present:
+        log = pd.concat(log_)
+    
+        temp = [0]*len(log)
+        for i in range(1, len(log)-1):
+            dt = log['time [HHMMSS.SSS]'].iloc[i+1] - log['time [HHMMSS.SSS]'].iloc[i-1]
+            dx = log['Crane Easting [m]'].iloc[i+1] - log['Crane Easting [m]'].iloc[i-1]
+            dy = log['Crane Northing [m]'].iloc[i+1] - log['Crane Northing [m]'].iloc[i-1]
+            speed = 3.6 * (math.sqrt(dx ** 2 + dy ** 2))/dt
+            temp[i] = speed
+            
+        log['Speed [km/h]'] = temp
+        log = log.sort_values(by=['#date [YYYYMMDD]', 'time [HHMMSS.SSS]'])
+        
+    else:
+        log = pd.DataFrame()
+        
+    ## pos ##
+    pos = pd.concat(pos_)
+    pos_result = pos.groupby(['Impact # [-]']).mean()
+    pos_result = pos_result.rename(columns={' X [m]' : 'X_average [m]',
+                                            ' Y [m]' : 'Y_average [m]'})
+    pos_result = pos_result.drop(' Pass # [-]', axis=1)
+    pos_first = pos.groupby(['Impact # [-]']).first()
+    pos_last = pos.groupby(['Impact # [-]']).last()
+    
+    list_direction = [0]*len(pos_result)
+    list_X_left = [0]*len(pos_result)
+    list_Y_left = [0]*len(pos_result)
+    list_X_right = [0]*len(pos_result)
+    list_Y_right = [0]*len(pos_result)
+    Pass_left = [0]*len(pos_result)
+    Pass_right = [0]*len(pos_result)
+    Impact_left = [0]*len(pos_result)
+    Impact_right = [0]*len(pos_result)
+    Speed = [0]*len(pos_result)
+    
+    wheel_dist = 1.03
+    
+    for i in range(1, len(pos_result)-1):
+        X_plus = pos_result['X_average [m]'].iloc[i+1]
+        X_min = pos_result['X_average [m]'].iloc[i-1]
+        Y_plus = pos_result['Y_average [m]'].iloc[i+1]
+        Y_min = pos_result['Y_average [m]'].iloc[i-1]
+        dx = abs(X_plus - X_min)
+        dy = abs(Y_plus - Y_min)
+        if dx == 0:
+            alpha = 0.5 * math.pi
+        else:
+            alpha = math.atan(dy/dx)
+        beta = (0.5 * math.pi) - alpha
+        if X_plus >= X_min and Y_plus >= Y_min:
+            dx_left = -wheel_dist * math.cos(beta)
+            dy_left = wheel_dist * math.sin(beta)
+            direction = beta
+        elif X_plus < X_min and Y_plus > Y_min:
+            dx_left = -wheel_dist * math.cos(beta)
+            dy_left = -wheel_dist * math.sin(beta)
+            direction = (2 * math.pi) - beta
+        elif X_plus > X_min and Y_plus < Y_min:
+            dx_left = wheel_dist * math.cos(beta)
+            dy_left = wheel_dist * math.sin(beta)
+            direction = (1 * math.pi) - beta
+        elif X_plus < X_min and Y_plus < Y_min:
+            dx_left = wheel_dist * math.cos(beta)
+            dy_left = -wheel_dist * math.sin(beta)
+            direction = beta + math.pi
+        
+        list_X_left[i] = pos_result['X_average [m]'].iloc[i] + dx_left
+        list_Y_left[i] = pos_result['Y_average [m]'].iloc[i] + dy_left
+        list_X_right[i] = pos_result['X_average [m]'].iloc[i] - dx_left
+        list_Y_right[i] = pos_result['Y_average [m]'].iloc[i] - dy_left
+        Pass_left[i] = pos_first[' Pass # [-]'].iloc[i]
+        Pass_right[i] = pos_last[' Pass # [-]'].iloc[i]
+        list_direction[i] = math.degrees(direction)
+        
+        impact_left = str(pos_result.index[i]) + 'L'
+        impact_right = str(pos_result.index[i]) + 'R'
+            
+        Impact_left[i] = impact_left
+        Impact_right[i] = impact_right        
+        
+        if log_present:
+            logtemp = log[['time [HHMMSS.SSS]', 'Speed [km/h]']].copy()
+            logtemp['time [HHMMSS.SSS]'] = abs(logtemp['time [HHMMSS.SSS]']-pos_result['time [HHMMSS]'].iloc[i])
+            logtemp = logtemp.sort_values(by='time [HHMMSS.SSS]')
+            Speed[i] = logtemp['Speed [km/h]'].iloc[0]
+                        
+    for i in [0, -1]:
+        Impact_left[i] = str(pos_result.index[i]) + 'L'
+        Impact_right[i] = str(pos_result.index[i]) + 'R'
+        list_X_left[i] = pos_result['X_average [m]'].iloc[i]
+        list_Y_left[i] = pos_result['Y_average [m]'].iloc[i]
+        list_X_right[i] = pos_result['X_average [m]'].iloc[i]
+        list_Y_right[i] = pos_result['Y_average [m]'].iloc[i]
+        Pass_left[i] = pos_first[' Pass # [-]'].iloc[i]
+        Pass_right[i] = pos_last[' Pass # [-]'].iloc[i]
+        
+    pos_result['#date [YYYYMMDD]'] = pd.to_datetime(pos_result['#date [YYYYMMDD]'], 
+                                                    format='%Y%m%d').dt.date
+    pos_result['time [HHMMSS]'] = pd.to_datetime(pos_result['time [HHMMSS]'], 
+                                                    format='%H%M%S').dt.time
+    
+    dict_left = {'Impact #' : Impact_left,
+            'Date' : list(pos_result['#date [YYYYMMDD]']),
+            'Time' : list(pos_result['time [HHMMSS]']),
+            'Acceleration' : list(pos_result[' Acceleration [m/s2]']),
+            'Direction [deg]' : list_direction,
+            'X' : list_X_left,
+            'Y' : list_Y_left,
+            'Pass' : Pass_left,
+            'Speed [km/h]' : Speed}
+    
+    dict_right = {'Impact #' : Impact_right,
+                  'Date' : list(pos_result['#date [YYYYMMDD]']),
+            'Time' : list(pos_result['time [HHMMSS]']),
+            'Acceleration' : list(pos_result[' Acceleration [m/s2]']),
+            'Direction [deg]' : list_direction,
+            'X' : list_X_right,
+            'Y' : list_Y_right,
+            'Pass' : Pass_right,
+            'Speed [km/h]' : Speed}
+    
+    df_left = pd.DataFrame(dict_left)
+    df_right = pd.DataFrame(dict_right)
+    output = df_left.append(df_right)
+    output = output.sort_values(by=['Date', 'Time', 'Impact #'])
+    output = output.set_index('Impact #')
+    
+    pos = pos.sort_values(by=['#date [YYYYMMDD]', 'time [HHMMSS]'])
+    
+    ## acc ##
+    if acc_present:
+        acc = pd.concat(acc_)
+        acc = acc.rename(columns={0 : '#date [YYYYMMDD]',
+                            1 : 'time [HHMMSS]',
+                            2 : 'Impact # [-]',
+                            3 : 'Heave acceleration [m/s2]'})
+        acc = acc.sort_values(by=['#date [YYYYMMDD]', 'time [HHMMSS]'])
+    else:
+        acc = pd.DataFrame()
+        
+    ## file name ##
+    #date = output['Date'].iloc[0]
+    #name_proc = 'CRC data_processed_' + str(date)
+    #name_raw = 'CRC data_raw_' + str(date)
+    
+    return output, pos, acc, log
+
+
+def show_preview(output):
+    st.write('**Preview:**')
+    fig = px.scatter(data_frame = output,
+                     x=output['X'],
+                     y=output['Y'],
+                     color='Pass', 
+                     color_continuous_scale='turbo')
+                     
+    fig.update_yaxes(scaleanchor='x', scaleratio=1)
+    st.write(fig)
