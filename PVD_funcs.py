ab37cdc8a03632b38bc4e871fd901d7cb695bbc0
KALT <75012967+kajalthuis@users.noreply.github.com>
2021-09-17 16:23:31 +0200

    Add files via upload

--- PVD_funcs.py
+++ PVD_funcs.py
@@ -12,62 +12,27 @@ import io
 import base64
 
 
-def convert(list_):
+def convert(list_, headerlist):
     
     frame = pd.concat(list_, axis=0, ignore_index=True)
     
         ## Rename columns ##
-    frame = frame.rename(columns={0: 'Date',
-                                  1: 'Time',
-                                  2: 'Easting [m]',
-                                  3: 'Northing [m]',
-                                  4: 'Height [m]',
-                                  5: 'Drain nr. [-]',
-                                  6: 'Job nr. [-]',
-                                  7: 'Base unit [-]',
-                                  8: 'Operator [-]',
-                                  9: 'Stitcher type [-]',
-                                  10: 'Stitcher length [m]',
-                                  11: 'Stitcher ballast [ton]',
-                                  12: 'Drain type [-]',
-                                  13: 'Anchoring [-]',
-                                  14: 'Pattern type [0=square/1=triang.]',
-                                  15: 'Pattern distance',
-                                  16: 'Pattern heading [deg]',
-                                  17: 'Pattern X-position [m]',
-                                  18: 'Pattern Y-position [m]',
-                                  19: 'Prescribed depth [m]',
-                                  20: 'Max. depth [m]',
-                                  21: 'Duration [s]',
-                                  22: 'Max. force [kN]',
-                                  23: 'Stitcher angle [deg]',
-                                  24: 'Remarks',
-                                  25: '',
-                                  26: '',
-                                  27: 'Log interval [m]',
-                                  28: 'Data nr [-]',
-                                  29: 'Force [kN]'})
-    #####
-    frame = frame.sort_values(['Date', 'Time'])
+    nums = list(range(len(headerlist)))
+    headerdict = dict(zip(nums, headerlist))    
+    frame = frame.rename(columns=headerdict)
+
+    frame = frame.sort_values(['date [YYYYMMDD]', 'time [HHMMSS]'])
     
         ## Add date and time columns ##
-    frame['Date'] = pd.to_datetime(frame['Date'], format='%Y%m%d').dt.date
-    frame['Time'] = pd.to_datetime(frame['Time'], format='%H%M%S').dt.time
+    frame['date [YYYYMMDD]'] = pd.to_datetime(frame['date [YYYYMMDD]'], format='%Y%m%d').dt.date
+    frame['time [HHMMSS]'] = pd.to_datetime(frame['time [HHMMSS]'], format='%H%M%S').dt.time
         
-        ## Convert columns to float ##
-    # frame.Num_blows = frame.Num_blows.astype(int)
-    # frame.Location = frame.Location.astype(int)
-    # frame.Easting = frame.Easting.astype(float)
-    # frame.Northing = frame.Northing.astype(float)
-    # frame.Total_settlement = frame.Total_settlement.astype(float)
-    # frame.Total_time = frame.Total_time.astype(float)
-    # frame.Total_energy = frame.Total_energy.astype(float)
-    # frame.Foot_diameter = frame.Foot_diameter.astype(float)
-    # frame.Stop_criterion = frame.Stop_criterion.astype(float)
-    # frame.Set_1 = frame.Set_1.astype(float)
+        ## Output on screen ##    
+    col1, col2 = st.columns(2)
+    col1.metric('Number of drains [-]', len(frame))
+    col2.metric('Linear meter [m]', round(frame['Max. depth [m]'].sum(),2))
     
-    st.write("  * Number of drains installed: ", len(frame))
-    st.write("  * Linear meter installed: ", round(frame['Max. depth [m]'].sum(),2))
+    
     
     return frame
 
@@ -75,8 +40,8 @@ def convert(list_):
 def show_preview(frame):
     st.write('**Preview:**')
     fig = px.scatter(data_frame = frame,
-                     x=frame['Easting [m]'],
-                     y=frame['Northing [m]'],
+                     x=frame['X [m]'],
+                     y=frame['Y [m]'],
                      color='Max. depth [m]', 
                      color_continuous_scale='turbo')
                      
